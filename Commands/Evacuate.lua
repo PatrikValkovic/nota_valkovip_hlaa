56577b6d9f6ddd39eab081ba21ea4fc675712042
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2020-05-22 19:22:14 +0200

    Dont with the TTDR homework

--- Commands/Evacuate.lua
+++ Commands/Evacuate.lua
@@ -32,22 +32,109 @@ local GetUnitPosition = Spring.GetUnitPosition
 local GetUnitTransporter = Spring.GetUnitTransporter
 local GetUnitIsTransporting = Spring.GetUnitIsTransporting
 local GiveOrderToUnit = Spring.GiveOrderToUnit --(unitid, CMD.MOVE, {target_x, target_y, target_z}, {})
+local GetUnitIsDead = Spring.GetUnitIsDead
 
 function Run(self, _, parameter)
   local plan = parameter.evacuation_plan
   local safetygrid = parameter.safetygrid
   local area = parameter.evac_area
-  
   for evacuator, evacuee in pairs(plan) do
-    local is_loaded = GetUnitIsTransporting(evacuator) ~= nil and #GetUnitIsTransporting(evacuator) > 0
-    if is_loaded and not self[evacuator] then
-      GiveOrderToUnit(evacuator, CMD.UNLOAD_UNITS, {area.center.x, area.center.y, area.center.z, area.radius}, {})
-      self[evacuator] = true
-    elseif is_loaded then
-      -- pass
-    else
-      GiveOrderToUnit(evacuator, CMD.LOAD_UNITS, {evacuee}, {})
-      self[evacuator] = nil
+    local isalive = not GetUnitIsDead(evacuator) and not GetUnitIsDead(evacuee)
+    if isalive then
+
+      if not self[evacuator] then
+        self[evacuator] = {state=0, prevstate=-1, same=0}
+      end
+      
+      if self[evacuator].state == 0 then -- just assigned command
+        local is_loaded = GetUnitIsTransporting(evacuator) ~= nil and #GetUnitIsTransporting(evacuator) > 0
+        if not is_loaded then
+          self[evacuator].state = 1
+        else
+          self[evacuator].state = 10
+        end
+      end
+      
+      if self[evacuator].state == 1 then -- should plan for pickup
+        local tarx, _, tarz = GetUnitPosition(evacuee)
+        local path = Sensors.ComputeEvacuatePaths(evacuator, tarx, tarz, safetygrid)
+        for i = 1, #path do
+          local modify = {}
+          if i ~= 1 then modify = {"shift"} end
+          GiveOrderToUnit(evacuator, CMD.MOVE, {path[i].x, path[i].y, path[i].z}, modify)
+        end
+        self[evacuator].target = path[#path]
+        self[evacuator].state = 2
+      end
+      
+      if self[evacuator].state == 2 then -- on the way to pickup
+        local target_x = self[evacuator].target.x
+        local target_z = self[evacuator].target.z
+        local curx, _, curz = GetUnitPosition(evacuator)
+        local dist = distance(curx, curz, target_x, target_z)
+        if dist < 100 then
+          self[evacuator].state = 3
+        end
+      end
+      
+      if self[evacuator].state == 3 then -- close to the pickup
+        GiveOrderToUnit(evacuator, CMD.LOAD_UNITS, {evacuee}, {})
+        self[evacuator].state = 4
+      end
+      
+      if self[evacuator].state == 4 then -- picking up
+        local is_loaded = GetUnitIsTransporting(evacuator) ~= nil and #GetUnitIsTransporting(evacuator) > 0
+        if is_loaded then
+          self[evacuator].state = 10
+        end
+      end
+
+      
+      if self[evacuator].state == 10 then -- carry something, should plan way back
+        local path = Sensors.ComputeEvacuatePaths(evacuator, area.center.x, area.center.z, safetygrid)
+        for i = 1, #path do
+          local modify = {}
+          if i ~= 1 then modify = {"shift"} end
+          GiveOrderToUnit(evacuator, CMD.MOVE, {path[i].x, path[i].y, path[i].z}, modify)
+        end
+        self[evacuator].target = path[#path]
+        self[evacuator].state = 11
+      end
+      
+      if self[evacuator].state == 11 then -- on the way to safety
+        local target_x = self[evacuator].target.x
+        local target_z = self[evacuator].target.z
+        local curx, _, curz = GetUnitPosition(evacuator)
+        local dist = distance(curx, curz, target_x, target_z)
+        if dist < 100 then
+          self[evacuator].state = 12
+        end
+      end
+      
+      if self[evacuator].state == 12 then -- close to the drop position
+        GiveOrderToUnit(evacuator, CMD.UNLOAD_UNITS, {area.center.x, area.center.y, area.center.z, area.radius}, {})
+        self[evacuator].state = 13
+      end
+      
+      if self[evacuator].state == 13 then -- droping
+        local is_loaded = GetUnitIsTransporting(evacuator) ~= nil and #GetUnitIsTransporting(evacuator) > 0
+        if not is_loaded then
+          self[evacuator].state = 0
+        end
+      end
+      
+      
+      --handle stuck
+      if self[evacuator].same > 20 then
+        self[evacuator].state = 0
+      end
+      if self[evacuator].state == self[evacuator].prevstate then
+        self[evacuator].same = self[evacuator].same + 1
+      else
+        self[evacuator].prevstate = self[evacuator].state
+        self[evacuator].same = 0
+      end
+    
     end
   end
   
--- Sensors/ComputeEvacuatePaths.lua
+++ Sensors/ComputeEvacuatePaths.lua
@@ -1,3 +1,120 @@
+-- SOURCE: https://gist.github.com/LukeMS/89dc587abd786f92d60886f4977b1953
+--[[  Priority Queue implemented in lua, based on a binary heap.
+Copyright (C) 2017 Lucas de Morais Siqueira <lucas.morais.siqueira@gmail.com>
+License: zlib
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgement in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]--
+
+local function priorityqueue() 
+  local floor = math.floor
+  local PriorityQueue = {}
+  PriorityQueue.__index = PriorityQueue
+  setmetatable(
+      PriorityQueue,
+      {
+          __call = function (self)
+              setmetatable({}, self)
+              self:initialize()
+              return self
+          end
+      }
+  )
+  function PriorityQueue:initialize()
+      --[[  Initialization.
+      Example:
+          PriorityQueue = require("priority_queue")
+          pq = PriorityQueue()
+      ]]--
+      self.heap = {}
+      self.current_size = 0
+  end
+  function PriorityQueue:empty()
+      return self.current_size == 0
+  end
+  function PriorityQueue:size()
+      return self.current_size
+  end
+  function PriorityQueue:swim()
+      -- Swim up on the tree and fix the order heap property.
+      local heap = self.heap
+      local floor = floor
+      local i = self.current_size
+
+      while floor(i / 2) > 0 do
+          local half = floor(i / 2)
+          if heap[i][2] < heap[half][2] then
+              heap[i], heap[half] = heap[half], heap[i]
+          end
+          i = half
+      end
+  end
+  function PriorityQueue:put(v, p)
+      --[[ Put an item on the queue.
+      Args:
+          v: the item to be stored
+          p(number): the priority of the item
+      ]]--
+      --
+
+      self.heap[self.current_size + 1] = {v, p}
+      self.current_size = self.current_size + 1
+      self:swim()
+  end
+  function PriorityQueue:sink()
+      -- Sink down on the tree and fix the order heap property.
+      local size = self.current_size
+      local heap = self.heap
+      local i = 1
+
+      while (i * 2) <= size do
+          local mc = self:min_child(i)
+          if heap[i][2] > heap[mc][2] then
+              heap[i], heap[mc] = heap[mc], heap[i]
+          end
+          i = mc
+      end
+  end
+  function PriorityQueue:min_child(i)
+      if (i * 2) + 1 > self.current_size then
+          return i * 2
+      else
+          if self.heap[i * 2][2] < self.heap[i * 2 + 1][2] then
+              return i * 2
+          else
+              return i * 2 + 1
+          end
+      end
+  end
+  function PriorityQueue:pop()
+      -- Remove and return the top priority item
+      local heap = self.heap
+      local retval = heap[1][1]
+      heap[1] = heap[self.current_size]
+      heap[self.current_size] = nil
+      self.current_size = self.current_size - 1
+      self:sink()
+      return retval
+  end
+  return PriorityQueue
+end
+
+
+
+
+
+
 local sensorInfo = {
 	name = "ComputeEvacuatePaths",
 	desc = "Compute evacuate paths.",
@@ -18,24 +135,93 @@ local function distance(firstx, firstz, secondx, secondz)
 end
 
 local GetUnitPosition = Spring.GetUnitPosition
+local Height = Spring.GetGroundHeight
 
-return function(toevacuate, safetygrid, evacuateposition, evacuate_distance)
-  evacuate_distance = evacuate_distance or 500
+return function(fromunit, tox, toz, grid)
+  local fromx, fromy, fromz = GetUnitPosition(fromunit)  
+  local step = 256
+  local mapX = Game.mapSizeX
+  local mapZ = Game.mapSizeZ
   
-  local relevant_to_evacuate = {}
-  for ev_i = 1, #toevacuate do
-    local x, _, z = GetUnitPosition(toevacuate[ev_i])
-    local dist = distance(x, z, evacuateposition[1], evacuateposition[3])
-    if dist < evacuate_distance then
-      relevant_to_evacuate[#relevant_to_evacuate + 1] = toevacuate[ev_i]
+  -- get closest points
+  local closest_from_x
+  local closest_from_z
+  local closest_from_distance = math.huge
+  local closest_to_x
+  local closest_to_z
+  local closest_to_distance = math.huge
+  for z, ztable in pairs(grid) do
+    for x, _ in pairs(ztable) do
+      local dist_from = distance(fromx, fromz, x, z)
+      local dist_to = distance(tox, toz, x, z)
+      if dist_from < closest_from_distance then
+        closest_from_distance = dist_from
+        closest_from_x = x
+        closest_from_z = z
+      end
+      if dist_to < closest_to_distance then
+        closest_to_distance = dist_to
+        closest_to_x = x
+        closest_to_z = z
+      end
+    end
   end
-  local evacuate_positions = {}
   
-  local paths = {}
+  local path = {
+    {x=fromx, y=fromy+25, z=fromz},
+    {x=closest_from_x, y=Height(closest_from_x, closest_from_z)+25, z=closest_from_z},
+  }
   
-  for unit_i = 1, #units do
+  
+  -- Dijkstra
+  local queue = priorityqueue()()
+  queue:put({x=closest_from_x, z=closest_from_z, dist=0, parent=nil}, 0)
+  local visited = {}
+  local found = false
+  local to_process
+  while not found and queue:size() > 0 do
+    to_process = queue:pop()
+    local already_processed = visited[to_process.z] and visited[to_process.z][to_process.x] 
+    if not already_processed then
+      if to_process.x == closest_to_x and to_process.z == closest_to_z then
+        found = true
+      else
+        local visitedztable = visited[to_process.z] or {} --visit node
+        visitedztable[to_process.x] = true
+        visited[to_process.z]=visitedztable
+        local following = {                       -- define children
+          {x=to_process.x-step, z=to_process.z},
+          {x=to_process.x+step, z=to_process.z},
+          {x=to_process.x, z=to_process.z-step},
+          {x=to_process.x, z=to_process.z+step},
+        }
+        for _, follow in pairs(following) do    -- traverse children
+          local isvalid = grid[follow.z] and grid[follow.z][follow.x]
+          if isvalid then
+            follow.dist = to_process.dist + 1 - grid[follow.z][follow.x] + 0.001
+            follow.parent = to_process
+            queue:put(follow, follow.dist)
+          end
+        end
+      end
+    end
+  end
+  
+  -- backtrack
+  if found then
+    local backway = {}
+    while to_process.parent do
+      backway[#backway + 1] = {x=to_process.x, y=Height(to_process.x, to_process.z), z=to_process.z}
+      to_process = to_process.parent
+    end
     
+    for i = 1, #backway do
+      path[#path + 1] = backway[#backway - i + 2]
+    end
   end
   
-  return paths
+
+  path[#path + 1] = {x=closest_to_x, y=Height(closest_to_x, closest_to_z)+25, z=closest_to_z}
+  path[#path + 1] = {x=tox, y=Height(tox, toz)+25, z=toz}
+  return path
 end
